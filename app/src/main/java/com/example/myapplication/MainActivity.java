9d2f76affa353b6c1e6dcfa495ad79c2a9b24f81
chechna9 <ja_bouchouareb@esi.dz>
2022-08-03 11:42:52 +0100

    switch between fragments

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,13 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.ItemSelected {
 
@@ -20,6 +15,7 @@ public class MainActivity extends AppCompatActivity implements ListFragment.Item
     private TextView tvDescription;
 
     String[] description;
+    FragmentManager manager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +23,30 @@ public class MainActivity extends AppCompatActivity implements ListFragment.Item
         setContentView(R.layout.main_activity);
 
         description = getResources().getStringArray(R.array.descriptions);
-//        onItemSelected(0);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            manager.beginTransaction()
+                    .show(manager.findFragmentById(R.id.listFrag))
+                    .hide(manager.findFragmentById(R.id.detailleFrag))
+                    .commit();
+        } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            manager.beginTransaction()
+                    .show(manager.findFragmentById(R.id.detailleFrag))
+                    .show(manager.findFragmentById(R.id.listFrag))
+                    .commit();
+        }
     }
 
 
     @Override
-    public  void onItemSelected(int index) {
+    public void onItemSelected(int index) {
         DetailleFrag.updateTv(description[index]);
-
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            manager.beginTransaction()
+                    .hide(manager.findFragmentById(R.id.listFrag))
+                    .show(manager.findFragmentById(R.id.detailleFrag))
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 }
